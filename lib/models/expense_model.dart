38a7f7aac9e97038f14410af861cd55f64b3561b
Amaanarabiyani <amaanmemon200@gmail.com>
2025-03-13 13:05:04 +0530

    First COmmit

---
+++ lib/models/expense_model.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ExpenseItem {
+  late String title;
+  late String amount;
+  late final DateTime dateTime;
+  ExpenseItem({
+    required this.title,
+    required this.amount,
+    required this.dateTime,
+  });
+}
